6a60fe4815673e0ead60f41d5a7e218b9f1bb089
Pratyush Goel <43815969+GoelPratyush@users.noreply.github.com>
2020-10-26 15:04:19 +0530

    Added Epsilon in tokenType

--- tokenizer.h
+++ tokenizer.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TOTAL_TOKEN_TYPES 29
+#define TOTAL_TOKEN_TYPES 30
 #define MAX_LEXEME_LEN 20     // Maximum number of characters in a lexeme
 
 // Make sure if you add a new name into the enum, update array enumValueToName
@@ -35,7 +35,8 @@ typedef enum {
     MUL,
     DIV,
     OR,
-    AND
+    AND,
+    EPSILON
 } TokenType;
 
 typedef struct token {
